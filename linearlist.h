9a2d0e2639362ecb5951eab990a3922d89f24f25
_Trancender <582865471@qq.com>
2015-09-16 00:00:08 +0800

    Create linearlist.h

---
+++ linearlist.h
@@ -0,0 +1,22 @@
+#ifndef LINEARLIST_H
+#define LINEARLIST_H
+
+#include <iostream>
+
+//线性表抽象类
+template <typename anytype>
+class LinearList
+{
+public:
+	virtual ~LinearList(){};
+	virtual bool empty() const = 0;
+	virtual int size() const = 0;
+	virtual anytype &get(int theIndex) const = 0;
+	virtual int indexOf(const anytype &theElement) const = 0;
+	virtual void erase(int theIndex) = 0;
+	virtual void insert(int theIndex, const anytype &theElement) = 0;
+	virtual void output(std::ostream &out) const = 0;	//把线性表插入输出流out
+};
+
+
+#endif	//linearlist.h
